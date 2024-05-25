3814133080836024ceec562d8909024dbaacbe44
Romain <romain.magne@edhec.com>
2024-05-25 16:45:11 +0200

    Merkle gift list

--- client/index.js
+++ client/index.js
@@ -4,11 +4,16 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const merkleTree = new MerkleTree(niceList);
+const name = 'Anna Stehr';
+const index = niceList.findIndex(n => n === name);
+const proof = merkleTree.getProof(index);
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
